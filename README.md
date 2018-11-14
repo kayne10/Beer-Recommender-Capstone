32f018012311d89da18a2c418455593083d3654d
kayne10 <troy.kayne@colorado.edu>
2018-11-13 17:19:41 -0700

    recommender works for a user input

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Beer Recommender
+
+## Summary
+
+## EDA
+
+
+## LDA Results
+
+(Elbow Plot?)
+
+| Topic Number     | Topics | Possible Category |
+| ---      | ---       |
+| 0 |'ale' 'pale' 'apa' 'amber' 'red' 'blonde' 'brown' 'wheat' 'saison' 'farmhouse'                 | APAs |
+| 1 |'ipa' 'double' 'imperial' 'hop' 'esb' 'bitter' 'extra' 'strong' 'belgian' 'session'                | IPAs |
+| 2 |'porter' 'baltic' 'coconut' 'doppelbock' 'vanilla' 'robust' 'plum' 'chocolate' 'java' 'voodoo'| Porters |
+| 3 |'pilsner' 'pilsener' 'german' 'czech' 'pils' 'barrel' 'grand' 'brew' 'golden' 'canyon'| Pilsners |
+| 4 |'white' 'witbier' 'märzen' 'wit' 'lager' 'house' 'king' 'street' 'bier' 'cold'| ? |
+| 5 |'belgian' 'stout' 'helles' 'oatmeal' 'black' 'light' 'munich' 'vienna' 'adjunct' 'milk'| Stouts|
+| 6 |'kölsch' 'winter' 'warmer' 'tripel' 'berliner' 'weissbier' 'island''shandy' 'summer' 'kentucky'| ? |
+| 7 |'beer' 'fruit' 'vegetable' 'rye' 'cream' 'pumpkin' 'wheat' 'great''spiced' 'herbed' | ? |
+| 8 |'cider' 'altbier' 'gold' 'apple' 'hard' 'dry' 'jack' 'ginger' 'miner' 'angry'| Ciders |
+
+## Input/Output Vectors
+
+| Topic probabilities | ABU | IBU|
+| --- | ---| --- |
+| | | |
+
+## Example
+```
+{'Hard Apple': ['Ginger Cider', "Wolfman's Berliner", 'Schilling Hard Cider', 'Hard Cider', 'Nomader Weiss', 'Monkey Chased the Weasel', 'Magic Apple', 'Contemplation', 'Totally Radler']}
+```
--- topic_modeler.py
+++ topic_modeler.py
@@ -10,6 +10,8 @@ from sklearn.model_selection import GridSearchCV, train_test_split
 
 # pd.options.display.max_columns = 30
 
+stop_words = ENGLISH_STOP_WORDS.union({'bock','style','scotch','california','oktoberfest','wee','special','english','american','hefeweizen','old','common','gose'})
+scaler = StandardScaler()
 
 class TopicModeler(object):
     """
@@ -24,6 +26,7 @@ class TopicModeler(object):
         self.model = model
         self.text = None
         self.names = None
+        self.target_vectors = None
         self.vectorizer = vectorizer
         self.feature_names = None
         self.doc_probs = None
@@ -55,11 +58,12 @@ class TopicModeler(object):
         """
 
         print('Vectorizing...')
+        #self.target_vectors = self.vectorizer.fit_transform(self.names) # not quite
         return self.vectorizer.fit_transform(self.text)
 
     def fit(self, text, names=None):
         """
-        Vectorizes corpus and fits model with it. Titles can be set here as well
+        Vectorizes corpus and fits model with it. Beers names are set here as well
 
         Args:
             text (iterable of strings): Array for documents
@@ -74,7 +78,7 @@ class TopicModeler(object):
         print('Fitting...')
         self.model.fit(self.word_vec)
         self.set_feature_names()
-        self.names = names
+        self.names = names # vectorize names and column stack abv and ibu
 
     def top_topic_features(self, num_features=10):
         """
@@ -129,13 +133,9 @@ class TopicModeler(object):
             np.array(int): array containing sorted indices of most similar documents
             """
 
-        # Append other features to doc_probs with a column_stack
-        vectors = np.column_stack((doc_probs, self.other_features))
-        # Use standard scaler on all vectors
-        scaler = StandardScaler()
+        vectors = np.column_stack((doc_probs, self.other_features)) # Append other features to doc_probs with a column_stack
         doc_probs = scaler.fit_transform(vectors)
-        # weigh features that are more important than others
-        doc_probs = doc_probs[:,-2:] * 2
+        # doc_probs = doc_probs[:,-2:] * 2 # weigh features that are more important than others
         distances = self.distance_func(doc_probs[doc_index, np.newaxis], doc_probs)
         return distances.argsort()
 
@@ -179,6 +179,22 @@ class TopicModeler(object):
         doc_probs = self.predict_proba(self.text)
         beer_similarities = self.sort_by_distance(doc_index, doc_probs)
         return self.find_closest_document_titles(beer_similarities, num_documents)
+
+    def recommend(self, user_input_data):
+        """Takes in a users input data that pipelines thru the process of fit transform recommend """
+        text = user_input_data['beer_name'] + ' ' + user_input_data['style']
+        vectorize_text = self.vectorizer.transform(text.split('  '))
+        input_doc_probs = self.model.transform(vectorize_text)
+        input_doc_probs = np.column_stack((input_doc_probs, user_input_data['abv'])) # not 6 rows :(
+        input_doc_probs = np.column_stack((input_doc_probs, user_input_data['ibu'])) # not 6 rows :(
+        input_doc_probs = scaler.fit_transform(input_doc_probs)
+        all_doc_probs = self.predict_proba(self.text)
+        all_doc_probs = np.column_stack((all_doc_probs, self.other_features))
+        all_doc_probs = scaler.fit_transform(all_doc_probs)
+        distances = self.distance_func(input_doc_probs, all_doc_probs)
+        sorted_distances = np.argsort(distances)
+        return self.find_closest_document_titles(sorted_distances,10) # modify find closest beers method
+
 
     def grid_search_lda(self, params):
         """
@@ -265,23 +281,37 @@ def load_data():
     return df
 
 
+def handle_input(fav_beer, fav_style, abv_pref, ibu_pref):
+    """ Takes a users preference of beer """
+    return {
+            'beer_name':fav_beer,
+            'style':fav_style,
+            'abv':abv_pref,
+            'ibu':ibu_pref
+    }
+
+
+
+
 def main():
     df = load_data() #add this make a sample .sample(100).reset_index(drop=True)
-    lda = LatentDirichletAllocation(n_components=7, learning_offset=50., verbose=1,
+    lda = LatentDirichletAllocation(n_components=9, learning_offset=50., verbose=1,
                                     doc_topic_prior=0.25, topic_word_prior=0.5, n_jobs=-1, learning_method='online')
-    tf_vectorizer = CountVectorizer(max_df=0.85, min_df=2, max_features=1000, stop_words='english')
+    tf_vectorizer = CountVectorizer(max_df=0.85, min_df=2, max_features=1000, stop_words=stop_words)
     tm = TopicModeler(lda, tf_vectorizer)
-
-    # tm.fit(df['beer_name'] + ' ' +  df['style'] + ' ' + df['brewery_name'], names=df['beer_name'])
-    tm.fit(df['style'], names=df['beer_name'])
+    tm.fit(df['beer_name'] + ' ' +  df['style'], names=df['beer_name'])
     tm.get_more_features(df)
-
     print(tm.top_topic_features())
 
-    print(tm.top_closest_beers(df.beer_name.iloc[5],20))
-    print(tm.top_closest_beers(df.beer_name.iloc[100],20))
-    print(tm.top_closest_beers(df.beer_name.iloc[500],20))
-    print(tm.top_closest_beers('Iron Mike Pale Ale',20))
+    user_preference = handle_input('Voodoo Ranger Imperial IPA','Imperial IPA',0.09,70.0)
+    # print(tm.recommend(user_preference))
+
+    print(tm.top_closest_beers(df.beer_name.iloc[17],10)) # imperial ipa
+    print(tm.top_closest_beers(df.beer_name.iloc[1090],10)) # cider
+    print(tm.top_closest_beers(df.beer_name.iloc[179],10)) # irish stout
+    print(tm.top_closest_beers('Iron Mike Pale Ale',10))
+    print("\n")
+    print(tm.recommend(user_preference))
 
     # Commented out because this can take a really long time!
     # params = {"n_components": [2, 5, 10, 50, 75, 100, 200]}
